db66ab0443b2d5c35287ffec8c6bd7eceb02788a
Ryan Patton <ryancpatton0@gmail.com>
2025-02-22 18:05:51 +0000

    update script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,14 +10,7 @@ ZIP_FILE="function.zip"
 # Build the Go application
 GOOS=linux GOARCH=amd64 go build
 
+ls
+
 # Package the application
 zip $ZIP_FILE main.go
-
-# Deploy to AWS Lambda
-aws lambda update-function-code --function-name $FUNCTION_NAME --zip-file fileb://$ZIP_FILE --region $REGION
-
-# Clean up
-rm main.go
-rm $ZIP_FILE
-
-echo "Deployment to AWS Lambda completed successfully."
